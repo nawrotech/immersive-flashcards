62d609b7a8ead0f49aa52e171df33e11defeea36
nawrotech <nawrot13@protonmail.com>
2025-02-15 07:17:18 +0100

    update deck creation form so the langauge can be selected

--- src/Repository/DeckRepository.php
+++ src/Repository/DeckRepository.php
@@ -16,26 +16,27 @@ class DeckRepository extends ServiceEntityRepository
         parent::__construct($registry, Deck::class);
     }
 
-    //    /**
-    //     * @return Deck[] Returns an array of Deck objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('d')
-    //            ->andWhere('d.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('d.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
 
-    //    public function findOneBySomeField($value): ?Deck
+
+    /**
+     * @return Deck[] Returns an array of Deck objects
+     */
+    public function findDecks($user): array
+    {
+        return $this->createQueryBuilder('d')
+            // ->andWhere('d.creator = :user')
+            // ->setParameter('user', $user)
+            ->orderBy('d.id', 'ASC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    //    public function findOneBySomeField($user): ?Deck
     //    {
     //        return $this->createQueryBuilder('d')
     //            ->andWhere('d.exampleField = :val')
-    //            ->setParameter('val', $value)
+    //            ->setParameter('val', $user)
     //            ->getQuery()
     //            ->getOneOrNullResult()
     //        ;
--- src/Service/LocaleMappingService.php
+++ src/Service/LocaleMappingService.php
@@ -10,7 +10,36 @@ class LocaleMappingService
 
     public function getMapping(string $locale): ?array
     {
-
         return $this->localeMappings[$locale] ?? null;
+    }
+
+    public function getLanguagesMapping()
+    {
+        return array_map(fn($item) => $item['language'], $this->localeMappings);
+    }
+
+    public function getServiceMappings(array $serviceKeys): array
+    {
+        $result = [];
+        foreach ($this->localeMappings as $locale => $data) {
+            foreach ($serviceKeys as $serviceKey) {
+                if (isset($this->localeMappings[$locale]['service_mappings'][$serviceKey])) {
+                    $result[$locale][$serviceKey] = $this->localeMappings[$locale]['service_mappings'][$serviceKey];
+                }
+            }
+        }
+
+        return $result;
+    }
+
+
+    public function getKeyMapping(string $locale, string $key): ?array
+    {
+        return $this->getMapping($locale)[$key] ?? null;
+    }
+
+    public function getServiceMapping(string $locale, string $serviceName): ?string
+    {
+        return $this->getKeyMapping($locale, 'service_mappings')[$serviceName];
     }
 }
