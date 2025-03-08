b1250a50dd85b54dfc0de6b5c9ee6cd24edc32d8
nawrotech <nawrot13@protonmail.com>
2025-03-08 09:02:35 +0100

    add test exception is thrown in GiphyApiService

--- tests/Unit/Service/GiphyApiServiceTest.php
+++ tests/Unit/Service/GiphyApiServiceTest.php
@@ -20,4 +20,39 @@ class GiphyApiServiceTest extends TestCase
         $result = $unsplashApiService->getImagesByQuery("", "en");
         $this->assertEmpty($result);
     }
+
+
+    public function testExceptionIsThrown(): void
+    {
+        $response = new JsonMockResponse(["data" => []], ["http_code" => 400]);
+        $client = new MockHttpClient([$response]);
+
+        $unsplashApiService = new GiphyApiService($client, "foo");
+
+        try {
+            $unsplashApiService->getImagesByQuery("ball", "en");
+        } catch (\RuntimeException $e) {
+            $this->assertEquals(400, $e->getCode());
+        }
+    }
+
+    // public function testSuccessfulResponse(): void
+    // {
+    //     $response = new JsonMockResponse(["results" => [
+    //         [
+    //             "urls" => [
+    //                 "small" => "localhost:8000"
+    //             ],
+    //             "id" => "foo",
+    //             "alt" => "soemthing",
+    //             "source" => "unsplash"
+    //         ]
+    //     ], ["http_code" => 200]]);
+
+    //     $client = new MockHttpClient([$response]);
+    //     $unsplashApiService = new GiphyApiService($client, "foo");
+    //     $result = $unsplashApiService->getImagesByQuery("blue", "en");
+
+    //     $this->assertCount(1, $result);
+    // }
 }
