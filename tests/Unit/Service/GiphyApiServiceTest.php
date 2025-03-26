fbb35f88d1660d55990bc7999de0b034175f7442
nawrotech <nawrot13@protonmail.com>
2025-03-26 07:39:21 +0100

    update tests for GiphyApiService

--- tests/Unit/Service/GiphyApiServiceTest.php
+++ tests/Unit/Service/GiphyApiServiceTest.php
@@ -7,52 +7,84 @@ use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpClient\MockHttpClient;
 use Symfony\Component\HttpClient\Response\JsonMockResponse;
 
-
 class GiphyApiServiceTest extends TestCase
 {
+    private const TEST_API_KEY = 'test_api_key';
+
+    protected function createServiceWithResponse(JsonMockResponse $response): GiphyApiService
+    {
+        $client = new MockHttpClient([$response]);
+        return new GiphyApiService($client, self::TEST_API_KEY);
+    }
+
     public function testEmptyResponse(): void
     {
         $response = new JsonMockResponse(["data" => [], ["http_code" => 200]]);
-        $client = new MockHttpClient([$response]);
+        $service = $this->createServiceWithResponse($response);
 
-        $unsplashApiService = new GiphyApiService($client, "foo");
-
-        $result = $unsplashApiService->getImagesByQuery("", "en");
+        $result = $service->getImagesByQuery("", "en");
         $this->assertEmpty($result);
     }
 
-
-    public function testExceptionIsThrown(): void
+    public function testLackOfLangParameterThrowsException(): void
     {
         $response = new JsonMockResponse(["data" => []], ["http_code" => 400]);
-        $client = new MockHttpClient([$response]);
-
-        $unsplashApiService = new GiphyApiService($client, "foo");
+        $service = $this->createServiceWithResponse($response);
 
         try {
-            $unsplashApiService->getImagesByQuery("ball", "en");
+            $service->getImagesByQuery("ball", "wrongLanguageCode");
+            $this->fail('Expected exception was not thrown');
         } catch (\RuntimeException $e) {
             $this->assertEquals(400, $e->getCode());
         }
     }
 
-    // public function testSuccessfulResponse(): void
-    // {
-    //     $response = new JsonMockResponse(["results" => [
-    //         [
-    //             "urls" => [
-    //                 "small" => "localhost:8000"
-    //             ],
-    //             "id" => "foo",
-    //             "alt" => "soemthing",
-    //             "source" => "unsplash"
-    //         ]
-    //     ], ["http_code" => 200]]);
+    public function testSuccessfulResponse(): void
+    {
+        $response = new JsonMockResponse(["data" => [
+            [
+                "id" => "foo",
+                "images" => [
+                    "original" => [
+                        "url" => "https://example.com/gif.gif"
+                    ]
+                ],
+                "alt_text" => "something",
+                "source" => "giphy"
+            ]
+        ], ["http_code" => 200]]);
 
-    //     $client = new MockHttpClient([$response]);
-    //     $unsplashApiService = new GiphyApiService($client, "foo");
-    //     $result = $unsplashApiService->getImagesByQuery("blue", "en");
+        $service = $this->createServiceWithResponse($response);
+        $result = $service->getImagesByQuery("blue", "en");
 
-    //     $this->assertCount(1, $result);
-    // }
+        $this->assertCount(1, $result);
+        $this->assertIsArray($result);
+        $this->assertNotEmpty($result);
+
+        $firstImage = $result[0];
+        $this->assertEquals("foo", $firstImage->id);
+        $this->assertEquals("https://example.com/gif.gif", $firstImage->url);
+        $this->assertEquals("something", $firstImage->alt);
+        $this->assertEquals("giphy", $firstImage->source);
+    }
+
+    public function testNetworkErrorHandling(): void
+    {
+        $response = new JsonMockResponse([], ['http_code' => 503]);
+        $service = $this->createServiceWithResponse($response);
+
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionCode(503);
+        $service->getImagesByQuery('query', 'en');
+    }
+
+    public function testInvalidJsonResponse(): void
+    {
+        $response = new JsonMockResponse('invalid json', ['http_code' => 200]);
+        $service = $this->createServiceWithResponse($response);
+
+        $this->expectException(\Symfony\Component\HttpClient\Exception\JsonException::class);
+        $this->expectExceptionMessage('JSON content was expected to decode to an array');
+        $service->getImagesByQuery('query', 'en');
+    }
 }
