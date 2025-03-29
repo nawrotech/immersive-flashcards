f47d9fdb2f0e70f70afb8cafed7e490637f59f15
nawrotech <nawrot13@protonmail.com>
2025-03-29 06:05:52 +0100

    update authentication process so the password is no longer needed

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -22,11 +22,6 @@ class RegistrationController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            /** @var string $plainPassword */
-            $plainPassword = $form->get('plainPassword')->getData();
-
-            // encode the plain password
-            $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
 
             $entityManager->persist($user);
             $entityManager->flush();
