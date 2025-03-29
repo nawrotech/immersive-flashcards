81472549424aa4f54edf8cb352d082888a911439
nawrotech <nawrot13@protonmail.com>
2025-03-29 13:13:35 +0100

    update practice template so images are correctly displayed

--- assets/controllers/flashcards-slider_controller.js
+++ assets/controllers/flashcards-slider_controller.js
@@ -1,120 +1,147 @@
-import { Controller } from '@hotwired/stimulus';
-
+import { Controller } from "@hotwired/stimulus";
 
 /* stimulusFetch: 'lazy' */
 export default class extends Controller {
+  static targets = [
+    "prevButton",
+    "nextButton",
+    "flashcardContainer",
+    "flashcard",
+    "video",
+    "rotateButton",
+  ];
 
-    static targets = ['prevButton', 'nextButton', 'flashcardContainer', 'flashcard'];
+  static values = {
+    numOfCards: Number,
+    storePracticeResultsUrl: String,
+    slidingTransitionDelay: String,
+  };
 
-    static values = {
-        numOfCards: Number,
-        storePracticeResultsUrl: String,
-        slidingTransitionDelay: String
+  result = [];
+  counter = 0;
+
+  connect() {
+    this.hideButtons();
+  }
+
+  next(delay = 0) {
+    if (this.counter < this.numOfCardsValue - 1) {
+      this.counter += 1;
+    }
+
+    this.slide(this.counter, delay);
+    this.hideButtons();
+  }
+
+  prev(delay = 0) {
+    if (this.counter >= 0) {
+      this.counter -= 1;
+    }
+    this.slide(this.counter, delay);
+    this.hideButtons();
+  }
+
+  slide(counter, delay) {
+    this.flashcardContainerTarget.style.setProperty("--counter", counter);
+    this.flashcardContainerTarget.style.setProperty("--delay", delay);
+  }
+
+  hideButtons() {
+    this.nextButtonTarget.classList.toggle(
+      "hidden",
+      this.counter >= this.numOfCardsValue - 1
+    );
+    this.prevButtonTarget.classList.toggle("hidden", this.counter <= 0);
+  }
+
+  rotate(event) {
+    if (!(event.currentTarget instanceof HTMLButtonElement)) return;
+
+    const button = event.currentTarget;
+    const flashcardId = button.dataset.flashcardId;
+
+    if (!flashcardId) return;
+
+    const card = this.flashcardTargets.find(
+      (card) => card.dataset.flashcardId === flashcardId
+    );
+    if (!card) return;
+
+    card.classList.toggle("rotated");
+
+    try {
+      const video = this.videoTargets.find(
+        (video) => video.dataset.flashcardId === flashcardId
+      );
+
+      if (video && video.tagName === "VIDEO") {
+        if (card.classList.contains("rotated")) {
+          video.play();
+        } else {
+          video.pause();
+          video.currentTime = 0;
+        }
+      }
+    } catch (e) {
+      console.log("No video found for flashcard or error:", e);
+    }
+  }
+
+  storeResult(id, result) {
+    const flashcardScore = {
+      id,
+      result,
     };
 
-    result = [];
-    counter = 0;
-
-    connect() {
-        this.hideButtons();
+    const existingIndex = this.result.findIndex((result) => result.id === id);
+    if (existingIndex !== -1) {
+      this.result[existingIndex] = flashcardScore;
+    } else {
+      this.result.push(flashcardScore);
     }
 
-    reverseSides() {
-        this.flashcardTargets.forEach(flashcard => flashcard.classList.toggle('inversed')); 
+    const lastFlashcardId = this.flashcardTargets.at(-1).dataset.flashcardId;
+    if (
+      lastFlashcardId == id &&
+      this.result.find((flashcard) => flashcard.id == lastFlashcardId)
+    ) {
+      this.sendResult();
     }
+  }
 
-    next(delay = 0) {
-        if (this.counter < this.numOfCardsValue - 1) {
-            this.counter += 1;
+  sendResult() {
+    fetch(this.storePracticeResultsUrlValue, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Requested-With": "XMLHttpRequest",
+      },
+      body: JSON.stringify(this.result),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.redirect) {
+          window.location.href = data.url;
         }
+      })
+      .catch((error) => console.error("Error fetching images:", error));
+  }
 
-        this.slide(this.counter, delay);
-        this.hideButtons();
-    }
+  correct(e) {
+    const practiceFlashcard = e.currentTarget.closest(".practice-flashcard");
+    const flashcardId = practiceFlashcard.dataset.flashcardId;
+    this.storeResult(flashcardId, "correct");
 
-    prev(delay = 0) {
-        if (this.counter >= 0) {
-            this.counter -= 1;
-        }
-        this.slide(this.counter, delay);
-        this.hideButtons();
-    }
+    practiceFlashcard.classList.add("correct", "active");
+    this.next(this.slidingTransitionDelayValue);
+  }
 
-    slide(counter, delay) {
-        this.flashcardContainerTarget.style.setProperty("--counter", counter);  
-        this.flashcardContainerTarget.style.setProperty("--delay", delay);  
-    }
+  incorrect(e) {
+    const practiceFlashcard = e.currentTarget.closest(".practice-flashcard");
+    const flashcardId = practiceFlashcard.dataset.flashcardId;
 
-    hideButtons() {
-        this.nextButtonTarget.classList.toggle('hidden', this.counter >= this.numOfCardsValue - 1);
-        this.prevButtonTarget.classList.toggle('hidden', this.counter <= 0);
-    }
-
-
-    rotate(e) {
-        e.currentTarget.closest('.practice-flashcard').classList.toggle('rotated');
-        console.log(e.currentTarget.closest('.practice-flashcard'));
-    }
-
-
-    storeResult(id, result) {
-        const flashcardScore = {
-            id,
-            result
-        };
-
-        const existingIndex = this.result.findIndex(result => result.id === id);
-        if (existingIndex !== -1) {
-          this.result[existingIndex] = flashcardScore;
-        } else {
-          this.result.push(flashcardScore);
-        }
-
-        const lastFlashcardId = this.flashcardTargets.at(-1).dataset.flashcardId;
-        if (lastFlashcardId == id && this.result.find(flashcard => flashcard.id == lastFlashcardId)) {
-            this.sendResult();
-        }
-    }
-
-    sendResult() {
-        fetch(this.storePracticeResultsUrlValue, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-Requested-With': 'XMLHttpRequest'
-            },
-            body: JSON.stringify(this.result,
-            )
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.redirect) {
-                window.location.href = data.url;
-            }
-        })
-        .catch(error => console.error('Error fetching images:', error));
-        ;
-    }
-
-    correct(e) {    
-        const practiceFlashcard = e.currentTarget.closest('.practice-flashcard');
-        const flashcardId = practiceFlashcard.dataset.flashcardId;
-        this.storeResult(flashcardId, 'correct');
-
-        practiceFlashcard.classList.add("correct", "active");
-        this.next(this.slidingTransitionDelayValue);
-
-    }
-
-    incorrect(e) {
-        const practiceFlashcard = e.currentTarget.closest('.practice-flashcard');
-        const flashcardId = practiceFlashcard.dataset.flashcardId;
-
-        this.storeResult(flashcardId, 'incorrect');
-        practiceFlashcard.classList.add("incorrect", "active");
-        this.next(this.slidingTransitionDelayValue);
-
-    }
-
-
+    this.storeResult(flashcardId, "incorrect");
+    practiceFlashcard.classList.add("incorrect", "active");
+    this.next(this.slidingTransitionDelayValue);
+  }
 }
