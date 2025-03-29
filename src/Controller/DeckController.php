9a6f2fc44abcdf5cae283a71701d44e12e3435f3
nawrotech <nawrot13@protonmail.com>
2025-03-29 06:29:14 +0100

    update decks template so the search input is added

--- src/Controller/DeckController.php
+++ src/Controller/DeckController.php
@@ -15,8 +15,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class DeckController extends AbstractController
