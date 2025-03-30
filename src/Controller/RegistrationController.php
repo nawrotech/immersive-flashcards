a5d429bfa283352ad5bbf1da5fcf366e9224c3b8
nawrotech <nawrot13@protonmail.com>
2025-03-30 11:55:22 +0200

    refactor registration controller so the verification email is sent

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,35 +4,91 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use App\Repository\UserRepository;
+use App\Security\EmailVerifier;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
 class RegistrationController extends AbstractController
 {
-    #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager): Response
+    public function __construct(private EmailVerifier $emailVerifier)
     {
+    }
+
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, EntityManagerInterface $entityManager): Response
+    {
+
+        if ($this->isGranted('ROLE_USER')) {
+            return $this->redirectToRoute('app_deck');
+        }
+
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-
             $entityManager->persist($user);
             $entityManager->flush();
 
-            $this->addFlash("success", "You're logged in!");
+            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+                (new TemplatedEmail())
+                    ->from(new Address('support@immersive-flashcards.tech', 'Support Immersive Flashcards'))
+                    ->to((string) $user->getEmail())
+                    ->subject('Please Confirm your Email')
+                    ->htmlTemplate('registration/confirmation_email.html.twig')
+            );
 
-            return $security->login($user, 'login_link', 'main');
+            $this->addFlash('success', 'Please check your email inbox and click on the confirmation link to complete your registration.');
+
+            return $this->redirectToRoute('app_deck');
         }
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form,
         ]);
+    }
+
+    #[Route('/verify/email', name: 'app_verify_email')]
+    public function verifyUserEmail(Request $request, 
+        TranslatorInterface $translator, 
+        UserRepository $userRepository,
+        EntityManagerInterface $em,
+        Security $security): Response
+    {
+        $id = $request->query->get('id');
+        if (null === $id) {
+            return $this->redirectToRoute('app_deck');
+        }
+
+        $user = $userRepository->find($id);
+        if (null === $user) {
+            return $this->redirectToRoute('app_deck');
+        }
+
+        $user->setIsVerified(true);
+        $em->persist($user);
+        $em->flush();
+
+        try {
+            $this->emailVerifier->handleEmailConfirmation($request, $user);
+        } catch (VerifyEmailExceptionInterface $exception) {
+            $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
+            return $this->redirectToRoute('app_register');
+        }
+
+        $security->login($user);
+
+        // @TODO Change the redirect on success and handle or remove the flash message in your templates
+        $this->addFlash('success', 'Your email address has been verified.');
+        return $this->redirectToRoute('app_deck');
     }
 }
