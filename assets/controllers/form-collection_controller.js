32b7616dcbd51df9ce3cb949c35aa37c19467c05
nawrotech <nawrot13@protonmail.com>
2025-04-03 16:22:56 +0200

    remove fetchLazy from all controllers

--- assets/controllers/form-collection_controller.js
+++ assets/controllers/form-collection_controller.js
@@ -1,7 +1,6 @@
 import { Controller } from '@hotwired/stimulus';
 
 
-/* stimulusFetch: 'lazy' */
 export default class extends Controller {
     static targets = ['collectionContainer', 'removeButton']
     static values = {
